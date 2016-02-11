13f6b2904084626cfe30e72564a54e6c84cb9a59
webunraveling <jason@webunraveling.com>
2016-02-11 11:30:35 -0600

    Updating license info

--- rss-leech.php
+++ rss-leech.php
@@ -9,6 +9,22 @@ Author URI:   http://webunraveling.com
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 
+/*
+RSS Leech
+Copyright (C) 2016 Jason Raveling
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+/*
 Not much happening here. Setting up this file structure to allow for future
 functionality to be added without having to change file structure when/if things
 become more complicated.
